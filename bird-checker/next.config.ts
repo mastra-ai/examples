9454faa79dbfec1d360f0b0824db1c368550b3d6
Kenny <adelekekehinde06@gmail.com>
2024-11-07 14:09:04 +0100

    add prisma workaround

--- bird-checker/next.config.ts
+++ bird-checker/next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import PrismaPlugin from "@prisma/nextjs-monorepo-workaround-plugin";
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -13,6 +14,13 @@ const nextConfig: NextConfig = {
   },
   outputFileTracingIncludes: {
     "**/*": ["./mastra/**/*"],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
   },
 };
 
