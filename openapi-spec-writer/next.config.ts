6de8ec6ced2085229b8cb6ef408f1155c20ce57e
Ehindero Israel <iehindero419@stu.ui.edu.ng>
2024-11-26 17:24:16 +0100

    Remove redundant package deps

--- openapi-spec-writer/next.config.ts
+++ openapi-spec-writer/next.config.ts
@@ -1,6 +1,4 @@
 import type { NextConfig } from "next";
-import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
-import CopyPlugin from "copy-webpack-plugin";
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -14,24 +12,7 @@ const nextConfig: NextConfig = {
     // your project has TypeScript errors.
     ignoreBuildErrors: true,
   },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins = [
-        ...config.plugins,
-        new PrismaPlugin(),
-        new CopyPlugin({
-          patterns: [
-            {
-              from: "./node_modules/@mastra/core/node_modules/@prisma-app/",
-              to: "./node_modules/@prisma-app/",
-            },
-          ],
-        }),
-      ];
-    }
 
-    return config;
-  },
   outputFileTracingIncludes: {
     "**/*": ["./mastra/**/*"],
   },
