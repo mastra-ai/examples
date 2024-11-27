6b8fd867096981ea6f10225ded13afb01cd678c0
Ehindero Israel <iehindero419@stu.ui.edu.ng>
2024-11-27 14:29:37 +0100

    Catch spec writer server action errors

--- openapi-spec-writer/src/actions/index.tsx
+++ openapi-spec-writer/src/actions/index.tsx
@@ -23,18 +23,23 @@ export async function generateOpenApiSpec({
       data: unknown;
     }
 > {
-  const res = await openApiSpecGenWorkflow.execute({
-    url,
-    pathRegex: crawlOptions.pathRegex,
-  });
+  try {
+    const res = await openApiSpecGenWorkflow.execute({
+      url,
+      pathRegex: crawlOptions.pathRegex,
+    });
 
-  console.log({
-    data: res.results["GENERATE_MERGED_SPEC"],
-  });
+    console.log({
+      data: res.results["GENERATE_MERGED_SPEC"],
+    });
 
-  const openApiSpec = (res.results["GENERATE_MERGED_SPEC"] as any)?.mergedSpec;
+    const openApiSpec = (res.results["GENERATE_MERGED_SPEC"] as any)
+      ?.mergedSpec;
 
-  return { message: "successful", data: openApiSpec };
+    return { message: "successful", data: openApiSpec };
+  } catch (e) {
+    return { message: "failed", data: JSON.stringify(e) };
+  }
 }
 
 export async function makeMastraPR({
@@ -46,17 +51,21 @@ export async function makeMastraPR({
   crawledUrl: string;
   integrationName: string;
 }) {
-  const res = await makePRToMastraWorkflow.execute({
-    integration_name: integrationName,
-    site_url: crawledUrl,
-    owner: "mastra",
-    repo: "mastra",
-    yaml,
-  });
+  try {
+    const res = await makePRToMastraWorkflow.execute({
+      integration_name: integrationName,
+      site_url: crawledUrl,
+      owner: "mastra",
+      repo: "mastra",
+      yaml,
+    });
 
-  const prUrl = (res.results["ADD_TO_GIT"] as any)?.pr_url;
+    const prUrl = (res.results["ADD_TO_GIT"] as any)?.pr_url;
 
-  const pr_url = prUrl;
+    const pr_url = prUrl;
 
-  return { message: "successful", data: pr_url };
+    return { message: "successful", data: pr_url };
+  } catch (e) {
+    return { message: "failed", data: JSON.stringify(e) };
+  }
 }
